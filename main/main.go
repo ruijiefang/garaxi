2d51d46c5e59148b46316e4165901eceaefe52db
Ruijie Fang <ruijief@princeton.edu>
2021-06-09 08:35:06 +0800

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/songgao/water"
+	"log"
+)
+
+func main() {
+	fmt.Printf("Hello world, Garaxi\n")
+	ifce, err := water.New(water.Config{
+		DeviceType: water.TUN,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Interface Name: %s\n", ifce.Name())
+
+	packet := make([]byte, 2000)
+	for {
+		n, err := ifce.Read(packet)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Packet Received: % x\n", packet[:n])
+	}
+}
---
+++ scripts/up.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -x
+sudo ifconfig $1 10.1.0.10 10.1.0.20 up
